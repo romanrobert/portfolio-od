2c1fc8abb79553f48d588351d9b8ecf6b3383f8c
romanrobert <iromanrobert@gmail.com>
2025-02-04 16:03:08 +0200

    added gsap animation for the hero section

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,16 @@ function setActiveNavLink() {
 setActiveNavLink();
 
 window.addEventListener("popstate", setActiveNavLink);
+
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.to(
+    ".hero-content .avatar, .hero-content .wrapper h1, .hero-content .wrapper p, .hero-content .wrapper button",
+    {
+      opacity: 1,
+      y: 0,
+      duration: 0.5,
+      stagger: 0.1,
+      ease: "power1.inOut",
+    }
+  );
+});
